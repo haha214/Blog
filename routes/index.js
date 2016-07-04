dac7c9530bd3e615a8a4229a308f29fd704f57dc
Xin <396460741@qq.com>
2016-07-04 16:46:38 +0800

    实现分页功能

--- routes/index.js
+++ routes/index.js
@@ -24,17 +24,23 @@ router.get('/', function(req, res, next) {
 
 // module.exports = router;
 module.exports = function(app){
-    app.get('/',function(req,res){
-        Post.getAll(null, function (err, posts) {
+    app.get('/', function(req, res) {
+        //判断是否是第一页，并把请求的页数转换成 number 类型
+        var page = parseInt(req.query.p) || 1;
+        //查询并返回第 page 页的 10 篇文章
+        Post.getTen(null, page, function(err, posts, total) {
             if (err) {
                 posts = [];
-            } 
-            res.render('index',{
-                title:'Express',
-                user:req.session.user,
+            }
+            res.render('index', {
+                title: '主页',
                 posts: posts,
-                success:req.flash('success').toString(),
-                error:req.flash('error').toString()
+                page: page,
+                isFirstPage: (page - 1) == 0,
+                isLastPage: ((page - 1) * 10 + posts.length) == total,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
             });
         });
     });
@@ -161,28 +167,33 @@ module.exports = function(app){
         res.redirect('/upload');
     });
 
-    app.get('/u/:name', function (req, res) {
+
+    app.get('/u/:name', function(req, res) {
+        var page = parseInt(req.query.p) || 1;
         //检查用户是否存在
-        User.get(req.params.name, function (err, user) {
+        User.get(req.params.name, function(err, user) {
             if (!user) {
-                req.flash('error', '用户不存在!'); 
-                return res.redirect('/');//用户不存在则跳转到主页
+                req.flash('error', '用户不存在!');
+                return res.redirect('/');
             }
-            //查询并返回该用户的所有文章
-            Post.getAll(user.name, function (err, posts) {
+            //查询并返回该用户第 page 页的 10 篇文章
+            Post.getTen(user.name, page, function(err, posts, total) {
                 if (err) {
-                    req.flash('error', err); 
+                    req.flash('error', err);
                     return res.redirect('/');
-                } 
+                }
                 res.render('user', {
                     title: user.name,
                     posts: posts,
-                    user : req.session.user,
-                    success : req.flash('success').toString(),
-                    error : req.flash('error').toString()
+                    page: page,
+                    isFirstPage: (page - 1) == 0,
+                    isLastPage: ((page - 1) * 10 + posts.length) == total,
+                    user: req.session.user,
+                    success: req.flash('success').toString(),
+                    error: req.flash('error').toString()
                 });
             });
-        }); 
+        });
     });
     app.get('/u/:name/:day/:title', function(req, res) {
         Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
