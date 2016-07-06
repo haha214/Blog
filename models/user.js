6fe77858755780f8b343b4797cc9058355068feb
Xin <396460741@qq.com>
2016-07-06 16:10:53 +0800

    添加头像

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongodb = require('./db');
+var crypto = require('crypto');
 
 function User(user) {
   this.name = user.name;
@@ -10,11 +11,15 @@ module.exports = User;
 
 //存储用户信息
 User.prototype.save = function(callback) {
-  //要存入数据库的用户文档
+  var md5 = crypto.createHash('md5'),
+    email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+    head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
+  //要存入数据库的用户信息文档
   var user = {
-      name: this.name,
-      password: this.password,
-      email: this.email
+    name: this.name,
+    password: this.password,
+    email: this.email,
+    head: head
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -35,7 +40,7 @@ User.prototype.save = function(callback) {
         if (err) {
           return callback(err);//错误，返回 err 信息
         }
-        callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
+        callback(null, user.ops[0]);//成功！err 为 null，并返回存储后的用户文档
       });
     });
   });
